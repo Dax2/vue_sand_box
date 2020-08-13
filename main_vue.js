1d9d25694e8546ca2c898d953b014a98a8fc0060
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-08-12 22:19:26 -0700

    Add files via upload

---
+++ main_vue.js
@@ -0,0 +1,15 @@
+// let app = new Vue ({
+//     el : '#app',
+//     data: {
+//         message:'you are wonderfull'
+//     }
+// })
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+  
