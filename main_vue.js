0972ae8efda7000b8465e6aa808952abc9b7482e
Daniel C <41881275+Dax2@users.noreply.github.com>
2020-12-02 18:20:40 -0800

    Update main_vue.js

--- main_vue.js
+++ main_vue.js
@@ -1,4 +1,5 @@
-// ok so the problem here is me. Need to format my prohects better. Not good enough understanding of file paths, trees etc and need fundamentals
+//looks like my road block is with formatting and setup.
+// ok so the problem here is me. Need to format my projects better. Not good enough understanding of file paths, trees etc and need fundamentals.
 // to grasp what the machine is actually doing. This type of knowledge improved when I was working with java. Perhaps some time spent there will help.
 
 
